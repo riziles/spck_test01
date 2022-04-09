8b2fb38a640ed7b2ff00f249bed6dcc4a81ec94c
michael.w.riley@gmail.com <michael.w.riley@gmail.com>
2022-04-08 18:29:32 -0600

    on a plane

--- app.js
+++ app.js
@@ -12,24 +12,32 @@ function Square(props) {
 
 
 class Board extends React.Component {
-  constructor(props) {    super(props);    this.state = {      squares: Array(9).fill(null),    };  }
+  constructor(props) {    
+    super(props);    
+    this.state = {      
+      squares: Array(9).fill(null),    
+      xIsNext: true,
+    };  
+  }
   
   renderSquare(i) {
     return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}
     />;  }
 
-handleClick(i) {    const squares = this.state.squares.slice();    squares[i] = 'X';    this.setState({squares: squares});  }
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
+handleClick(i) {    
+  
+  const squares = this.state.squares.slice();
+  squares[i] = this.state.xIsNext ? 'X' : 'O';
+  this.setState({
+    squares: squares,
+    xIsNext: !this.state.xIsNext,
+  });
+  
   }
   
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (
+      this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
